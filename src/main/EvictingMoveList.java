2b90df38f88f63226e50817191129655a4947472
matjojo <mat2904jojo@gmail.com>
2020-07-03 23:58:02 +0200

    Implement EvictingMoveList

--- src/main/EvictingMoveList.java
+++ src/main/EvictingMoveList.java
@@ -5,30 +5,50 @@ import org.jetbrains.annotations.NotNull;
 
 public class EvictingMoveList {
 
-	private final int size;
+	private final int length;
 	private final Move[] backing;
 
 	public EvictingMoveList(int size) {
-		this.size = size;
+		this.length = size;
 		this.backing = new Move[size];
 	}
 
 	@Contract(pure = true)
 	private EvictingMoveList(Move @NotNull [] backing) {
 		this.backing = backing;
-		this.size = backing.length;
+		this.length = backing.length;
+	}
+
+	/**
+	 * @return the topmost element in this list. So the one most recently added.
+	 */
+	public Move mostRecent() {
+		return getOffsetFromFront(0);
+	}
+
+	/**
+	 * @return the second most recent Move, so the one before the most recently added.
+	 */
+	public Move secondMostRecent() {
+		return getOffsetFromFront(1);
 	}
 
 	public void add(@NotNull Move move) {
 		// from the first to one from the last, so if len=3 we do 0 <= 1, 1<= 2, 2<= newMove
-		for (int i = 0; i < this.size - 1; i++) { // <size-2 is the same as <=size - 2;
+		for (int i = 0; i < this.length - 1; i++) { // <size-2 is the same as <=size - 2;
 			backing[i] = backing[i+1];
 		}
-		backing[size-1] = move;
+		backing[length -1] = move;
 	}
 
 	public EvictingMoveList copy() {
 		return new EvictingMoveList(this.backing.clone());
+	}
+
+	public Move getOffsetFromFront(int offset) {
+		int index = (length - offset) - 1;
+		if (index < 0) throw new IllegalArgumentException("Offset too large! MoveList is of length: " + length + ", offset was: " + offset);
+		return backing[index];
 	}
 
 	public void printContent() {
