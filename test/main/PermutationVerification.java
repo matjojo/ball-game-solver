52aac9f08c2b556b7225fa139888ae3883681b9e
matjojo <mat2904jojo@gmail.com>
2020-07-06 19:50:11 +0200

    TEST: fic tests to work with new solver and add game level 78 to tests

--- test/main/PermutationVerification.java
+++ test/main/PermutationVerification.java
@@ -18,53 +18,67 @@ public class PermutationVerification {
 	@Test
 	public void testSolvedBoards() {
 		Board solved = BoardOf(BucketOf());
-		assertCanBeSolved(solved);
+		assertCanBeSolved(5, solved);
 		solved = BoardOf(BucketOf(1,1,1,1));
-		assertCanBeSolved(solved);
+		assertCanBeSolved(5, solved);
 		solved = BoardOf(BucketOf(1,1,1,1), BucketOf(2,2,2,2));
-		assertCanBeSolved(solved);
+		assertCanBeSolved(5, solved);
 		solved = BoardOf(BucketOf(1,1,1,1), BucketOf(2,2,2,2), BucketOf(3,3,3,3));
-		assertCanBeSolved(solved);
+		assertCanBeSolved(5, solved);
 	}
 
 	@Test
 	public void testOneMoveBoards() {
+		GameSolver solver = new GameSolver();
 		Board oneMoveAway = BoardOf(BucketOf(1,1,1), BucketOf(1));
-		assertCanBeSolved(oneMoveAway);
+		assertCanBeSolved(1, oneMoveAway);
 		oneMoveAway = BoardOf(BucketOf(1,1,1,1), BucketOf(2,2,2), BucketOf(2));
-		assertCanBeSolved(oneMoveAway);
+		assertCanBeSolved(1, oneMoveAway);
 		oneMoveAway = BoardOf(BucketOf(1,1,1), BucketOf(2,2,2,2), BucketOf(1));
-		assertCanBeSolved(oneMoveAway);
+		assertCanBeSolved(1, oneMoveAway);
 	}
 
 	@Test
 	public void testTwoMoveBoards() {
 		Board twoMovesAway = BoardOf(BucketOf(1,1), BucketOf(1,1));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 		twoMovesAway = BoardOf(BucketOf(1,1), BucketOf(1), BucketOf(1));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 		twoMovesAway = BoardOf(BucketOf(2,2,2,2), BucketOf(1,1), BucketOf(1), BucketOf(1));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 		twoMovesAway = BoardOf(BucketOf(1,1), BucketOf(2,2,2,2), BucketOf(1), BucketOf(1));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 		twoMovesAway = BoardOf(BucketOf(1,1), BucketOf(1), BucketOf(2,2,2,2), BucketOf(1));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 		twoMovesAway = BoardOf(BucketOf(1,1), BucketOf(1), BucketOf(1), BucketOf(2,2,2,2));
-		assertCanBeSolved(twoMovesAway);
+		assertCanBeSolved(2, twoMovesAway);
 	}
 
 	@Test
 	public void testManyMoveBoards() {
 		Board manyAway = BoardOf(BucketOf(1,1,1,2), BucketOf(2,2,2,1));
-		assertCanBeSolved(manyAway);
+		assertCanBeSolved(20, manyAway);
+
 		manyAway = BoardOf(
 				BucketOf(2, 1, 1, 1),
 				BucketOf(2, 3, 3, 3),
 				BucketOf(2, 4, 4, 4),
 				BucketOf(2, 1, 3, 4));
-		assertCanBeSolved(manyAway);
-	}
+		assertCanBeSolved(20, manyAway);
 
+		manyAway = BoardOf(
+				BucketOf(7, 4, 5, 2),
+				BucketOf(1, 2, 10, 6),
+				BucketOf(10, 10, 2, 8),
+				BucketOf(2, 4, 3, 7),
+				BucketOf(6, 6, 4, 5),
+				BucketOf(3, 6, 1, 7),
+				BucketOf(5, 5, 8, 3),
+				BucketOf(4, 1, 8, 10),
+				BucketOf(3, 1, 7, 8)
+		);
+		assertCanBeSolved(50, manyAway);
+	}
 
 	// *********** //
 	// unsolvables //
@@ -72,25 +86,25 @@ public class PermutationVerification {
 	@Test
 	public void testSimpleUnsolvables() {
 		Board unsolvable = BoardOf(BucketOf(1));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10, unsolvable);
 		unsolvable = BoardOf(BucketOf(1,1));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10, unsolvable);
 		unsolvable = BoardOf(BucketOf(1,1,1));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10, unsolvable);
 		unsolvable = BoardOf(BucketOf(1,2));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10, unsolvable);
 		unsolvable = BoardOf(BucketOf(1,2,3));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10, unsolvable);
 		unsolvable = BoardOf(BucketOf(1,2,3,4));
-		assertCantBeSolved(unsolvable);
+		assertCantBeSolved(10 , unsolvable);
 	}
 
-	private static void assertCantBeSolved(Board shouldBeSolved) {
-		Assertions.assertFalse(BallGameSolverMain.solve(shouldBeSolved, dummyList()));
+	private static void assertCantBeSolved(int limit, Board shouldBeSolved) {
+		Assertions.assertFalse(new GameSolver().solve(shouldBeSolved, dummyList(), limit));
 	}
 
-	private static void assertCanBeSolved(Board shouldBeSolved) {
-		Assertions.assertTrue(BallGameSolverMain.solve(shouldBeSolved, dummyList()));
+	private static void assertCanBeSolved(int limit, Board shouldBeSolved) {
+		Assertions.assertTrue(new GameSolver().solve(shouldBeSolved, dummyList(), limit));
 	}
 
 	private static EvictingMoveList dummyList() {
